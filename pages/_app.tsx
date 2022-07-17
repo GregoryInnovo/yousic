f18bcffe1dd8fa26c0e95167246b85eae8b75fb1
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-07-17 17:37:16 -0500

    [ADD] sidebar part 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,14 @@ import PlayerLayout from "../components/playerLayout";
 import "reset-css";
 
 const theme = extendTheme({
-  color: {
+  colors: {
     gray: {
-      100: "#F5F5F5",
-      200: "#E5E5E5",
+      100: "#F5f5f5",
+      200: "#EEEEEE",
       300: "#E0E0E0",
       400: "#BDBDBD",
       500: "#9E9E9E",
-      660: "#757575",
+      600: "#757575",
       700: "#616161",
       800: "#424242",
       900: "#212121",
