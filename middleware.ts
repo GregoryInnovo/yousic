ad4b920357a35193e4ea5286b3f951a30846a65b
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-07-18 01:31:41 -0500

    [ADD] edgeFunction middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const signedinPages = ["/", "/playlist", "/library"];
+
+export function middleware(req: NextRequest) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    console.log(token);
+
+    if (!token) {
+      const url = req.nextUrl.clone();
+      console.log(url);
+      url.pathname = "/signin";
+      url.search = "";
+      return NextResponse.redirect(url);
+    }
+  }
+}
