f36ad751c50025423b143cc4d45a71ec058cb09c
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-07-19 18:42:33 -0500

    [ADD] GradientLayout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayput";
+
 const Home = () => {
-  return <div>homeeeeeeeeeeeeeeeeeeee</div>;
+  return (
+    <GradientLayout
+      color="blue"
+      subtitle="profile"
+      title="Gregory Innovo"
+      description="15 public playlist"
+      image="https://i1.sndcdn.com/avatars-8HrCNIyDmIBiWIH2-sUB0ug-t500x500.jpg"
+      roundImage
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
