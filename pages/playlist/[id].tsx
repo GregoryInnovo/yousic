26d03a8e8da4dc6acd806082b5bc9d30c9079a8c
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-07-23 18:44:47 -0500

    [ADD] format data

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -29,7 +29,7 @@ const Playlist = ({ playlist }) => {
       description={`${playlist.songs.lenght} songs`}
       image={`https://picsum.photos/400?random=${playlist.id}`}
     >
-      <SongTable />
+      <SongTable songs={playlist.songs} />
     </GradientLayout>
   );
 };
