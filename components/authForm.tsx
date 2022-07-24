281808ee9ac20d3ac9c04610e4034618c0e23710
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-07-24 00:28:51 -0500

    [ADD] configs to deploy

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -28,7 +28,7 @@ const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
         height="100px"
         borderBottom="white 1px solid"
       >
-        <NextImage src="/logo.png" height={60} width={180} />
+        <NextImage src="/logo.jpeg" height={120} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box padding="50px" bg="gray.900" borderRadius="6px">
